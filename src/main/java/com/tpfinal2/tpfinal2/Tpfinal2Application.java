abb6a49ba14cc033ffaf26a75e7f90d7f62ac887
BrianZvic <zvicbrian@gmail.com>
2023-12-08 14:36:58 -0300

    repo inicial

---
+++ src/main/java/com/tpfinal2/tpfinal2/Tpfinal2Application.java
@@ -0,0 +1,13 @@
+package com.tpfinal2.tpfinal2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tpfinal2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tpfinal2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tpfinal2/tpfinal2/Tpfinal2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpfinal2.tpfinal2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tpfinal2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
